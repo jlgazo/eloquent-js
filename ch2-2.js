faad2bdde9eb2de7e330f3b289f385b406c87fd3
Jan <jan@sym.ph>
2015-06-16 13:17:32 +0800

    Add Exercise 2.2

---
+++ ch2-2.js
@@ -0,0 +1,16 @@
+for(var count=1;count<=100;count++){
+	if(count%3==0){
+		if(count%5==0){
+			console.log("FizzBuzz");
+		}
+		else{
+			console.log("Fizz");
+		}
+	}
+	else if(count%5==0){
+		console.log("Buzz");
+	}
+	else{
+		console.log(count);
+	}
+}
